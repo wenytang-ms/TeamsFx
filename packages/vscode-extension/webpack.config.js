f181cc3285091b07d41e087b4943691a4cbf69e2
wenyutang-ms <wenyutang@microsoft.com>
2022-12-05 13:59:45 +0800

    chore: udpate

--- packages/vscode-extension/webpack.config.js
+++ packages/vscode-extension/webpack.config.js
@@ -90,7 +90,7 @@ const config = {
       "@npmcli/node-gyp"
     ),
     new DefinePlugin({
-      "process.env": buildConfig.preview.env,
+      "process.env.TEAMSFX_V3": JSON.stringify('true')
     }),
     new CopyPlugin({
       patterns: [
